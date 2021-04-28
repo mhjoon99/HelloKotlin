82f7042e33fda73a7f3b79932768c1e306fa93f1
Hye-joon Ma <hyejoon8@gmail.com>
2021-04-28 10:52:12 +0900

    Java+Kotlin buttons

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.hellokotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+//    val btnHello = findViewById<Button>(R.id.btnJava)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //var btnHello : Button   //var->일반적인 변수   val->값이 정해져 있음 ==const, final
-        val btnHello = findViewById<Button>(R.id.btnHello)
-        //btnHello = findViewById(R.id.btnHello)
-        btnHello.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Hello Kotlin", Toast.LENGTH_LONG).show()
+        val btnJava = findViewById<Button>(R.id.btnJava)
+        val btnKotlin = findViewById<Button>(R.id.btnKotlin)
+
+        btnJava.setOnClickListener {
+            startActivity(Intent(this@MainActivity, BmiJavaActivity::class.java))
+        }
+        btnKotlin.setOnClickListener {
+            startActivity(Intent(this@MainActivity, BmiKotlinActivity::class.java))
         }
     }
 }

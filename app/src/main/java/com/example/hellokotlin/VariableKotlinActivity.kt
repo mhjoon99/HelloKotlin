f479ccb4b171d94ef6f224c34359b7382919ad4c
Hye-joon Ma <hyejoon8@gmail.com>
2021-04-29 15:50:15 +0900

    Time elapsed

--- app/src/main/java/com/example/hellokotlin/VariableKotlinActivity.kt
+++ app/src/main/java/com/example/hellokotlin/VariableKotlinActivity.kt
@@ -16,6 +16,7 @@ class VariableKotlinActivity : AppCompatActivity() {
         val txtActivityStartTime = findViewById<TextView>(R.id.txtActivityStartTime)
         val txtCountBtnClicks = findViewById<TextView>(R.id.txtCountBtnClicks)
         val btnClickMe = findViewById<Button>(R.id.btnClickMe)
+        val txtElapsedTime = findViewById<TextView>(R.id.txtElapsedTime)
 
         var clickCount = 0
         val startTime = System.currentTimeMillis()
@@ -26,6 +27,8 @@ class VariableKotlinActivity : AppCompatActivity() {
         btnClickMe.setOnClickListener {
             clickCount++
             txtCountBtnClicks.text = "Button clicks = ${clickCount}"
+            var elapsedSeconds : Long = ((System.currentTimeMillis() - startTime) / 1000.0).toLong()
+            txtElapsedTime.text = "${elapsedSeconds} Seconds elapsed"
         }
     }
 }
